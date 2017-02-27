680c36b45ba47f8cdf27a28642f7250313ea6534
Troels Fedder Jensen <tfj@ase.au.dk>
2017-02-27 12:46:05 +0100

    Starting point for example

--- ECS/Control.cs
+++ ECS/Control.cs
@@ -1,28 +1,8 @@
-﻿namespace ECS
+﻿using System.Net.Configuration;
+
+namespace ECS
 {
     public class Control
     {
-        private IHeater _heater;
-        private ITempSensor _tempSensor;
-        private int _threshold;
-
-        public Control(IHeater heater, ITempSensor tempSensor, int threshold)
-        {
-            _heater = heater;
-            _tempSensor = tempSensor;
-            _threshold = threshold;
-        }
-
-        public bool RunSelfTest()
-        {
-            return _heater.RunSelfTest() && _tempSensor.RunSelfTest();
-        }
-
-        public void Regulate()
-        {
-            var temp = _tempSensor.GetTemp();
-            if (temp < _threshold) _heater.TurnOn();
-            else _heater.TurnOff();
-        }
     }
 }
